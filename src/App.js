e60dd7ef36a200714e2b2aa6678a1da0ba4df49d
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2023-02-17 17:27:12 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,28 +8,29 @@ import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import EditUserPage from "./pages/EditUserPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import Books from "./pages/Books";
+
 //import IsPrivate from "./components/IsPrivate";  
 //import IsAnon from "./components/IsAnon";
 //import BookListPage from "./pages/BookListPage";
 function App() {
   return (
     <div className="App">
-    
+    <Router> 
       <Navbar />
 
       <Routes>      
         <Route exact path="/" element={<HomePage />} />   
         <Route path="*" element={ <ErrorPage /> } />
         {/*<Route exact path="/books" element={<BookListPage />} />*/}
-
-       <Route path="/signup" element={ <SignupPage /> } /> 
+        <Route path="/books" element={ <Books /> } /> 
+        <Route path="/signup" element={ <SignupPage /> } /> 
        <Route path="/login" element={ <LoginPage /> } />
        <Route path="/profilePage" element={ <UserProfilePage/> } />
        <Route path="/users/edit" element={ <EditUserPage /> } />
-        {/* <Route path="/login" element={ <IsAnon> <LoginPage /> </IsAnon> } /> */} 
-       
+        {/* <Route path="/login" element={ <IsAnon> <LoginPage /> </IsAnon> } /> */}
       </Routes>
-     
+      </Router> 
     </div>
   );
 }
