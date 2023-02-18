26b3dd20a72f2b2ef29d526b1c73b1d3daae1ed3
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2023-02-18 11:15:29 +0100

    Add files via upload

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import {AuthContext} from "../context/auth.context"
 
-const API_URL = "http://localhost:5005";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005"; 
  
 function NewBook() {
     const { book, setUser, removeToken, storeToken } = useContext(AuthContext);
