95f6c5117ae84f0b9acb99b774281f528f8d97e1
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2023-03-03 17:14:18 +0100

    Add files via upload

--- src/pages/BookPage.js
+++ src/pages/BookPage.js
@@ -47,12 +47,12 @@ const handleSubmit = (e) => {
     }
 
 return (
-    <div>
+    <div className='AddBookPage'>
     <div>
     <h4>Title: {book.title}</h4>
     <img src={imgURL}  alt="book" style={{ width: '200',  height: '300px'}}/>
     <br/>
-        <ul>
+        <ul className="FormBlock">
             <li>Description: {book.description}</li>
             <li>Author: {book.author.username}</li>
 
@@ -75,14 +75,14 @@ return (
         <div>
         <div>        
         <br/>
-        <Link to="/books"><button>Back to books list</button></Link>
-        {book &&  (<Link to={`/books/edit/${book._id}`}><button>Edit BOOK</button></Link>)}
+        <Link to="/books"><button className="PageButton">Back to books list</button></Link>
+        {book &&  (<Link to={`/books/edit/${book._id}`}><button className="PageButton">Edit BOOK</button></Link>)}
         </div> 
         </div>
         <form onSubmit={handleSubmit}>
         <textarea type="text" value={comment} onChange={(e) => setComment(e.target.value)} name="comment" placeholder='Comment'></textarea>
         <span className="name">{book.name}</span>
-        <button type="submit">Create comment</button>
+        <button className="PageButton" type="submit">Create comment</button>
         </form>
         <div>
         
@@ -91,7 +91,7 @@ return (
  {book.comments && book.comments.map(comment => {
 
             return (
-            <div key ={comment._id} > {comment.author.username}  : {comment.comment}</div>
+            <div  key ={comment._id} > {comment.author.username}  : {comment.comment}</div>
             
         )
     })
